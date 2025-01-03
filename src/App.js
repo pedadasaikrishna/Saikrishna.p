e9e68404888e5849f80bfbbb0d0544fca7051ee5
sai krishna <160296180+pedadasaikrishna@users.noreply.github.com>
2025-01-03 22:43:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,3 @@
-// import styled, { ThemeProvider } from "styled-components";
-// import { darkTheme } from "./utils/Themes";
-// import Navbar from "./components/Navbar";
-// import { BrowserRouter } from "react-router-dom";
-// import Hero from "./components/sections/Hero";
-// import Skills from "./components/sections/Skills";
-// import Projects from "./components/sections/Projects";
-// import Education from "./components/sections/Education";
-// import Contact from "./components/sections/Contact";
-// import Experience from "./components/sections/Experience";
-// import Footer from "./components/sections/Footer";
-
-// const Body = styled.div`
-//   background-color: ${({ theme }) => theme.bg};
-//   width: 100%;
-//   overflow-x: hidden;
-//   position: relative;
-// `;
-
-// const Wrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   padding: 0 20px;
-//   margin-top: 20px;
-// `;
-
-
-// function App() {
-//   return (
-//     <ThemeProvider theme={darkTheme}>
-//       <BrowserRouter>
-//         <Navbar />
-//         <Body>
-//           <Wrapper>
-//             <Hero />
-//             </Wrapper>
-//             <Skills />
-          
-
-//           <Experience />
-//           <Projects />
-//           <Education />
-//           <Contact />
-//           <Footer />
-      
-//         </Body>
-//       </BrowserRouter>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
 
 
 
@@ -75,6 +15,7 @@ import Contact from './components/sections/Contact';
 import Experience from './components/sections/Experience';
 import Footer from './components/sections/Footer';
 import Album from './components/sections/Album.jsx'; // Import the Album component
+import LikeDislike from './components/sections/LikeDislike.jsx';
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -106,6 +47,7 @@ const router = createBrowserRouter([
             <Projects />
             <Education />
             <Contact />
+            <LikeDislike/>
             <Footer />
           </Wrapper>
         </Body>
